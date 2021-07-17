7d34e015f77295ee896e2bce2ac439c8153bc346
uilegendsteam@gmail.com <uilegendsteam@gmail.com>
2021-07-17 12:25:24 +0530

    change in checkbox and radio button

--- scripts.js
+++ scripts.js
@@ -1,16 +1,30 @@
-var name = document.getElementById('name').value;
-var email = document.getElementById('email').value;
-var getButtonId = document.getElementById('submitId');
-var checkBoxs = document.querySelectorAll('input[type=checkbox]');
+const checkBoxes = document.querySelectorAll('input[type=checkbox]');
+const radios = document.querySelectorAll('input[type=radio]');
+const submitBtn = document.getElementById('formSubmit');
 
-function getcheckedValues() {
- const allChecked = Array.from(checkBoxs).filter((ck) => ck.checked).map((ck) => ck.value);
- if (allChecked && allChecked.length !== 0) {
-  return allChecked;
+submitBtn.addEventListener('click', () => {
+ const checkboxSelected = Array.from(checkBoxes).filter((ck) => ck.checked).map((ck) => ck.value);
+ const radioSelected = Array.from(radios).find(radio => radio.checked);
+
+ if (checkboxSelected && checkboxSelected.length > 0) {
+  document.getElementById('showResult').innerHTML = checkboxSelected;
+  document.getElementById('checkboxValidate').style.display = 'none';
+  submitBtn.removeAttribute("disabled");
  } else {
-  return null;
+  document.getElementById('checkboxValidate').innerHTML = "Please select at least one value";
+  document.getElementById('showResult').innerHTML = '';
  }
-}
-getButtonId.addEventListener('click', () => {
- document.getElementById('showResult').innerHTML = getcheckedValues();
+
+ if (radioSelected && radioSelected.value) {
+  document.getElementById('result').innerHTML = radioSelected.value;
+  document.getElementById('mandatory').style.display = 'none';
+ } else {
+  document.getElementById('mandatory').innerHTML = 'Please select any one!';
+ }
+
 });
+
+
+function resetForm() {
+ window.location.reload();
+}
